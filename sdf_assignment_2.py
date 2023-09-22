a05c77ea381089077e3935f3856f3b031f458f6a
tbhullar2 <tbhullar2@acadamic.rrc.ca>
2023-09-22 15:34:50 -0500

    simple data types

---
+++ sdf_assignment_2.py
@@ -0,0 +1,36 @@
+"""
+Description: Resubmission of Assignment 2
+Author: Tanvir Singh Bhullar
+Date: 22/09/2023
+Usage:Python and Variables
+"""
+
+
+#SIMPLE DATA TYPES
+
+
+name = "Tanvir Singh Bhullar"
+print("value:",name)
+print(type(name))
+
+Manitoba_License= "True"
+print("value:",Manitoba_License)
+print(type(Manitoba_License))
+
+year= 2023
+print("value:",year)
+print(type(year))
+
+next_year= year + 1
+print("next:", next_year, "type", type(next_year))
+
+#CALCULATIONS
+
+#LISTS
+
+#TUPLES
+
+#DICTIONARIES
+
+#SETS
+
